3a3d8ef5015bbad7d1c9ffc4741845ee19ca0dc4
Nir Beck <nir@beckermus.com>
2018-01-09 21:42:12 +0200

    Add form for messegaes

--- public/js/index.js
+++ public/js/index.js
@@ -15,4 +15,26 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+// socket.emit('createMessage',{
+//   from:'Frank',
+//   text: 'Hi'
+// }, function(callbackdata){
+//   console.log('Got It ',callbackdata);
+// });
+
+
+jQuery('#message-form').on('submit',function(e){//listener like emit
+e.preventDefault();
+socket.emit('createMessage',{
+  from:'User',
+  text: jQuery('[name=message]').val()
+},function (){
+
+});
 });
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
+const utils = require(__dirname +'/utils/message');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
@@ -21,26 +22,30 @@ io.on('connection', (socket) => {
   // });
 
 
-socket.emit('newMessage',{//emit from admin text welcome to chat app
-  from:'Admin',
-  text:'Welcome to the chat app'
-});
+// socket.emit('newMessage',{//emit from admin text welcome to chat app
+//   from:'Admin',
+//   text:'Welcome to the chat app'
+// });
 
+socket.emit('newMessage',utils.generateMessage('Admin','Welcome to the chat app'));
+socket.broadcast.emit('newMessage',utils.generateMessage('Admin','New user joined'));
 //socket.broadcast.emit from admin text new user joined
-socket.broadcast.emit('newMessage',{
-  from:'Admin',
-  text:'New user joined',
-  createAt:new Date().getTime()
-});
+// socket.broadcast.emit('newMessage',{
+//   from:'Admin',
+//   text:'New user joined',
+//   createAt:new Date().getTime()
+// });
 
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage',{//IO IS EMIT FOR EVERYONE CONNECTED
       from:message.from,
       text:message.text,
       createAt:new Date().getTime()
     });
+
+    callback('this is from server.');
 
 
 // socket.broadcast.emit('newMessage',{//emit to everyone exept the user sent the event
